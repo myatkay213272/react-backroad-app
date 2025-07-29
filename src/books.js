ec3e49081c42fe3f7d4003fa15ec317a9c39f384
mkzt <myatthant68@gmail.com>
2025-07-29 15:03:07 +0630

    first commit

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/book-1.jpg'
+import img2 from './images/book-2.jpg'
+import img3 from './images/book-3.jpg'
+
+const books =[
+  {
+    id:1,
+    img : img1,
+    author:'Jorden Moore',
+    title : 'Wedding-People-Novel'
+  },
+  {
+    id:2,
+    img : img2,
+    author : 'Jorden Clear',
+    title : 'My first learing workbook'
+  },{
+    id:3,
+    img : img3,
+    author:'Meghan Quinn',
+    title : 'Till Summer Do Us Part'
+  }
+]
+
+export default books
