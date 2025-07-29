3ca69c3878597621c3484eee377eeff3ab4634b9
mkzt <myatthant68@gmail.com>
2025-07-29 15:10:11 +0630

    ...

--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'
-import books from './books'
-import Book from './Book'
-
-const BookList =()=>{
- 
-  return (
-    <>
-    <h1>amazon best sellers</h1>
-
-    <section className='booklist'>
-     {
-      books.map((book,index)=>(
-        <Book key={book.id} {...book} number={index} />
-      ))
-     }
-    </section> 
-  
-    </>
-  )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
